e22e9196b763868cbc4c04d50013fe8bcec57cbf
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-03 19:34:36 +0100

    Add Profile component

--- src/jsx/components/App.jsx
+++ src/jsx/components/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import userData from './profile/userdata.json';
+import Profile from './profile/Profile';
 
 export default function App() {
   return (
     <div>
-      <h2>my react template</h2>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 }
