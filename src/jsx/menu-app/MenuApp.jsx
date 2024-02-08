234aba187b81f2bb469e14aa7f27d9e86ea893eb
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-08 18:11:41 +0100

    refactoring

--- src/jsx/menu-app/MenuApp.jsx
+++ src/jsx/menu-app/MenuApp.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { NavLink, Routes, Route } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
+import { useState } from 'react';
 import { BookPhone } from '../components/bookphone/BookPhone';
 import { CafeFeedback } from '../components/cafe-feedback/CafeFeedback';
 import { ProfileSection } from '../components/profile-section/PofileSection';
@@ -6,23 +8,49 @@ import { ProfileSection } from '../components/profile-section/PofileSection';
 import styles from './MenuApp.module.css';
 import clsx from 'clsx';
 
-const buildLinkClass = ({ isActive }) => {
-  return clsx(styles.link, isActive && styles.active);
-};
-
 export const MenuApp = () => {
+  const [activeLink, setActiveLink] = useState('/');
+
+  const handleNavLinkClick = to => {
+    setActiveLink(to);
+  };
+
+  const buildLinkClassName = to => {
+    return clsx(styles.link, to === activeLink && styles.active);
+  };
+
   return (
     <div className="container">
       <nav className={styles.menu}>
-        <NavLink className={buildLinkClass} to="/">
+        <NavLink
+          className={buildLinkClassName('/')}
+          to="/"
+          onClick={() => handleNavLinkClick('/')}
+        >
           Profile
         </NavLink>
 
-        <NavLink className={buildLinkClass} to="/cafefeedback">
+        <NavLink
+          className={buildLinkClassName('/cafefeedback')}
+          to="/cafefeedback"
+          onClick={() => handleNavLinkClick('/cafefeedback')}
+        >
           Cafe Feedback
         </NavLink>
 
-        <NavLink className={buildLinkClass} to="/bookphone">
+        <NavLink
+          className={buildLinkClassName('/bookphone')}
+          to="/bookphone"
+          onClick={() => handleNavLinkClick('/bookphone')}
+        >
+          Book Phone
+        </NavLink>
+
+        <NavLink
+          className={buildLinkClassName('/bookphone')}
+          to="/bookphone"
+          onClick={() => handleNavLinkClick('/bookphone')}
+        >
           Book Phone
         </NavLink>
       </nav>
@@ -32,6 +60,12 @@ export const MenuApp = () => {
         <Route path="/cafefeedback" element={<CafeFeedback />} />
         <Route path="/bookphone" element={<BookPhone />} />
       </Routes>
+
+      {/* <Switch>
+        <Route path="/cafefeedback" element={<CafeFeedback />} />
+        <Route path="/bookphone" element={<BookPhone />} />
+        <Route path="/" element={<ProfileSection />} />
+      </Switch> */}
     </div>
   );
 };
