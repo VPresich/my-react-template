3cfc688a3dafd7b000988a89aa4f8ad7d9ce24f9
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-12 14:33:50 +0100

    add dark filter for cads

--- src/jsx/components/gallery/image-gallery/ImageCard.jsx
+++ src/jsx/components/gallery/image-gallery/ImageCard.jsx
@@ -3,7 +3,10 @@ import styles from './ImageCard.module.css';
 export const ImageCard = ({ image: { urls, description } }) => {
   return (
     <div className={styles.card}>
-      <img className={styles.img} src={urls.small} alt={description} />
+      <div className={styles.imgContainer}>
+        <img className={styles.img} src={urls.small} alt={description} />
+        <div className={styles.imgDarkened}></div>
+      </div>
     </div>
   );
 };
